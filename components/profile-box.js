ef0dbc74f8515a79a46214735d0060b6bbc6b0ed
erendoru <erendoru@hotmail.com>
2020-08-18 01:34:35 +0300

    Profil buton componenti eklendi

---
+++ components/profile-box.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import cn from "classnames";
+import styles from "./profile-box.module.css";
+import { ArrowBottom } from "./icons";
+import Photo from "./photo";
+import Button from "./button";
+import TextBody from "./text-body";
+
+function ProfileBox({ slug = "erendoru", name = "Eren Doru" }) {
+  return (
+    <Button className={cn(styles.box)}>
+      <Photo />
+      <div className={styles.body}>
+        <TextBody bold>{name}</TextBody>
+        <TextBody className={styles.slug}>@{slug}</TextBody>
+      </div>
+      <ArrowBottom className={styles.icon} />
+    </Button>
+  );
+}
+
+export default ProfileBox;
